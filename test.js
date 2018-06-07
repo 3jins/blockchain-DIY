618eb649d1de1e806bdefa9512ac0a30c28a701a
Sejin Jeon <jinaidy93@gmail.com>
2018-06-07 17:36:31 +0900

    Add timebench functionality to test.js

--- test.js
+++ test.js
@@ -2,13 +2,25 @@
 import Blockchain from './Blockchain';
 import Block from './Block';
 
+const checkTimeSpent = (callback, role) => {
+    let start = new Date();
+    callback();
+    console.log(((new Date() - start) / 1000) + "seconds has spent to " + role + ".");
+};
+
 let testChain = new Blockchain(4);
 
 testChain.createGenesisBlock();
-testChain.addBlock(new Block(1, new Date(), { content: "test" }));
+checkTimeSpent(
+    () => testChain.addBlock(new Block(1, new Date(), { content: "test" })),
+    "add a new block"
+);
 console.log(testChain.isChainValid());
 setTimeout(() => {
-        testChain.addBlock(new Block(2, new Date(), {content: "test2"}));
+        checkTimeSpent(
+            () => testChain.addBlock(new Block(2, new Date(), {content: "test2"})),
+            "add a new block"
+        );
         console.log(testChain.isChainValid());
         // testChain.getLatestBlock().hash = "Love is the most beautiful when it cannot come true";
         // console.log(testChain.isChainValid());
